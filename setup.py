3bfe6c56ccee480c3a54d4eee747030a35b86c41
BedirhanDurmus <bedirhan_durmus@hotmail.com>
2024-08-09 19:08:54 +0300

    Setup

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages,setup
 from typing import List
 
 HYPEN_E_DOT='-e .'
-def get_requirements(file_path:str)->Lisst[str]:
+def get_requirements(file_path:str)->List[str]:
     '''
     this function will return a list of requirements
     '''
