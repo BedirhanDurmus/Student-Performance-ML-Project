62380f3a613a00dc90dfaeb9a5bb781bb4735f99
BedirhanDurmus <bedirhan_durmus@hotmail.com>
2024-08-09 19:02:44 +0300

    Setup

---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import find_packages,setup
+from typing import List
+
+HYPEN_E_DOT='-e .'
+def get_requirements(file_path:str)->Lisst[str]:
+    '''
+    this function will return a list of requirements
+    '''
+    requirements=[]
+    with open('requirements.txt') as file_obj:
+        requirements=file_obj.readlines()
+        requirements=[req.replace("\n"," ") for req in requirements]
+
+        
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+            
+            
+    return requirements
+
+
+setup(
+
+name='mlproject',
+version='0.0.1',
+author='Bedirhan',
+author_email='bedirhan_durmus@hotmail.com',
+packages=find_packages(),
+install_requires=get_requirements('requirements.txt'),
+     
+)
